402b361a8a0b5f58d2af393996c640689bf696ed
e1616033 <150251429+lachupriya0993@users.noreply.github.com>
2025-11-09 06:10:32 +0000

    completed ex 2

--- ex1.py
+++ ex1.py
@@ -1,3 +1,10 @@
 def find(s, n):
-# write your implementation here
-    return None
+    for i in range (len(s)):
+        for j in range(i+1,len(s)):
+            if s[i]+s[j]==n:
+              return [i,j]
+
+target = find( [2,7,11,15],9)
+print(target)
+print(find([3,3],6))
+print(find([3,2,4],6))
